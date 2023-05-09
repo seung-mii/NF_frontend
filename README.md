5876e4e8e22f2b81d5710b8851d932fb8e7c257e
withsme <tmdal9953@gmail.com>
2023-05-09 16:10:51 +0900

    Feat 초기 설정

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# NF_frontend
+# NF_frontend 
+
+## 실행방법
+ 1. `npm install` 실행하여 node_modules 폴더를 다운
+ 2. `npm start` 실행
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      Success! 
+    </div>
+  );
+}
+
+export default App;

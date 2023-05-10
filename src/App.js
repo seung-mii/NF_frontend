00f11410d112c16918835bfdc1485bead1821511
withsme <tmdal9953@gmail.com>
2023-05-10 10:53:45 +0900

    Design NotFound 페이지

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home.js';
 import Make from './Pages/Meeting/Make.js';
 import Board from './Pages/Meeting/Board.js';
 import Check from './Pages/Restaurant/Check.js';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/make" element={<Make />}></Route>
         <Route path="/check" element={<Check />}></Route>
         <Route path="/board" element={<Board />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './NotFound.css';
+
+function NotFound() {
+  return (
+    <div style={{ margin: "20px" }}>
+      <h1>페이지를 찾을 수 없습니다.</h1>
+      <a href='/'> 홈으로 </a>
+    </div>
+  );
+}
+
+export default NotFound;

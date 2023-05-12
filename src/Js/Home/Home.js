8ba3c04b91ac24711223a6ce4de785ae55c5fb11
withsme <tmdal9953@gmail.com>
2023-05-12 16:31:03 +0900

    Feat 홈페이지 백엔드 연동 및 분류별 리스트 변경

--- src/Js/Home/Home.js
+++ src/Js/Home/Home.js
@@ -1,8 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import '../../Css/Home/Home.css';
 import profile from '../../Images/profile.png';
+import { call } from '../../Service/ApiService';
 
 function Home() {
+  const [categroy, setCategroy] = useState("전체");
+  const [entireType, setEntireType] = useState(true);
+  const [koreanType, setKoreanType] = useState(false);
+  const [schoolType, setSchoolType] = useState(false);
+  const [chineseType, setChineseType] = useState(false);
+  const [japaneseType, setJapaneseType] = useState(false);
+  const [westernType, setWesternType] = useState(false);
+  const [cafeType, setCafeType] = useState(false);
+  const [midnightType, setMidnightType] = useState(false);
+  const [list, setList] = useState([]);
+  const [entireList, setEntireList] = useState([]);
+  const [koreanList, setKoreanList] = useState([]);
+  const [schoolList, setSchoolList] = useState([]);
+  const [chineseList, setChineseList] = useState([]);
+  const [japaneseList, setJapaneseList] = useState([]);
+  const [westernList, setWesternList] = useState([]);
+  const [cafeList, setCafeList] = useState([]);
+  const [midnightList, setMidnightList] = useState([]);
+
+  const allTypeOff = () => { 
+    setEntireType(false);
+    setKoreanType(false);
+    setSchoolType(false);
+    setChineseType(false);
+    setJapaneseType(false);
+    setWesternType(false);
+    setCafeType(false);
+    setMidnightType(false);
+  };
+
+  const onEntireClick = () => { 
+    setCategroy("전체")
+    setList(entireList);
+    allTypeOff();
+    setEntireType(true);
+  };
+  const onKoreanClick = () => { 
+    setCategroy("한식")
+    setList(koreanList);
+    allTypeOff();
+    setKoreanType(true);
+  };
+  const onSchoolClick = () => { 
+    setCategroy("분식")
+    setList(schoolList);
+    allTypeOff();
+    setSchoolType(true);
+  };
+  const onChineseClick = () => { 
+    setCategroy("중식")
+    setList(chineseList);
+    allTypeOff();
+    setChineseType(true);
+  };
+  const onJapaneseClick = () => { 
+    setCategroy("일식")
+    setList(japaneseList);
+    allTypeOff();
+    setJapaneseType(true);
+  };
+  const onWesternClick = () => { 
+    setCategroy("양식")
+    setList(westernList);
+    allTypeOff();
+    setWesternType(true);
+  };
+  const onCafeClick = () => { 
+    setCategroy("카페")
+    setList(cafeList);
+    allTypeOff();
+    setCafeType(true);
+  };
+  const onMidnightClick = () => { 
+    setCategroy("야식")
+    setList(midnightList);
+    allTypeOff();
+    setMidnightType(true);
+  };
+
+  useEffect(() => {
+    call("/api/board/getList", "GET", null).then((response) => { setList(response.data); });
+    call("/api/board/getList", "GET", null).then((response) => { setEntireList(response.data); });
+    call("/api/board/getListByCategory?category=한식", "GET", null).then((response) => { setKoreanList(response.data); });
+    call("/api/board/getListByCategory?category=분식", "GET", null).then((response) => { setSchoolList(response.data); });
+    call("/api/board/getListByCategory?category=중식", "GET", null).then((response) => { setChineseList(response.data); });
+    call("/api/board/getListByCategory?category=일식", "GET", null).then((response) => { setJapaneseList(response.data); });
+    call("/api/board/getListByCategory?category=양식", "GET", null).then((response) => { setWesternList(response.data); });
+    call("/api/board/getListByCategory?category=카페", "GET", null).then((response) => { setCafeList(response.data); });
+    call("/api/board/getListByCategory?category=야식", "GET", null).then((response) => { setMidnightList(response.data); });
+  }, []);
+
+  // console.log(list);
+  
   return (
     <div className='home'>
       <div className='header'>
@@ -12,38 +107,40 @@ function Home() {
       <hr />
       <div className='main'>
         <div className='type'>
-          <p>전체</p>
-          <p>한식</p>
-          <p>분식</p>
-          <p>중식</p>
-          <p>일식</p>
-          <p>양식</p>
-          <p>카페</p>
-          <p>야식</p>
+          <p className={`${entireType ? 'this' : ''}`} onClick={onEntireClick}>전체</p>
+          <p className={`${koreanType ? 'this' : ''}`} onClick={onKoreanClick}>한식</p>
+          <p className={`${schoolType ? 'this' : ''}`} onClick={onSchoolClick}>분식</p>
+          <p className={`${chineseType ? 'this' : ''}`} onClick={onChineseClick}>중식</p>
+          <p className={`${japaneseType ? 'this' : ''}`} onClick={onJapaneseClick}>일식</p>
+          <p className={`${westernType ? 'this' : ''}`} onClick={onWesternClick}>양식</p>
+          <p className={`${cafeType ? 'this' : ''}`} onClick={onCafeClick}>카페</p>
+          <p className={`${midnightType ? 'this' : ''}`} onClick={onMidnightClick}>야식</p>
         </div>
-        <div className='board'>
-          <div className='user'>
-            <img src={profile} alt='profile' />
-            <div className='infor'>
-              <p className='name'>김민지</p>
-              <p className='date'>2023년 1월 23일</p>
+        {list && list.map((item) => 
+          <div className='board' id={item.board_no} key={item.board_no}>
+            <div className='user'>
+              <img src={profile} alt='profile' />
+              <div className='infor'>
+                <p className='name'>{item.member.name}</p>
+                <p className='date'>{item.reg_date.slice(0, 4)}년 {item.reg_date.slice(5, 7)}월 {item.reg_date.slice(8, 10)}일</p>
+              </div>
+            </div>
+            <h4 className='title'>{item.title}</h4>
+            <p className='content'>{item.contents}</p>
+            <div className='location'>
+              <span class="material-symbols-rounded">location_on</span>
+              <p>경상북도 구미시 대학로 62 오름관 1동 1층</p>
+            </div>
+            <div className='restaurant'>
+              <span class="material-symbols-rounded">home</span>
+              <p>{item.restaurant.name}</p>
+            </div>
+            <div className='btn'>
+              <Link to={`/board/${item.board_no}`}><button>댓글 작성</button></Link>
+              <button className='primary'>모집 {item.cur_people}/{item.max_people}명</button>
             </div>
           </div>
-          <h4 className='title'>로제떡볶이 드실 분</h4>
-          <p className='content'>곱창떡볶이도 좋습니다 사실 떡볶이는 다 좋아요 글쓰면서 배고파진다....</p>
-          <div className='location'>
-            <span class="material-symbols-rounded">location_on</span>
-            <p>경상북도 구미시 대학로 62 오름관 1동 1층</p>
-          </div>
-          <div className='restaurant'>
-            <span class="material-symbols-rounded">home</span>
-            <p>배떡 옥계점</p>
-          </div>
-          <div className='btn'>
-            <a href='/board'><button>댓글 작성</button></a>
-            <button className='primary'>모집 2/4명</button>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );

395dc78db98aa32a24f7327be536eb0e754d4768
withsme <tmdal9953@gmail.com>
2023-05-21 02:11:23 +0900

    Feat 모임 게시글 댓글 생성 및 삭제 기능

--- src/Js/Meeting/Board.js
+++ src/Js/Meeting/Board.js
@@ -1,38 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import '../../Css/Meeting/Board.css';
 import profile from '../../Images/profile.png';
 import { call } from '../../Service/ApiService';
+import * as AppStorage from "../../AppStorage";
 
 function Board() {
   const { id } = useParams();
   const [list, setList] = useState([]);
   const [reply, setReply] = useState({
-    reply_no: 0,
-    contents: "",
-    reg_date: "",
-    mod_date: "",
-    writer: ""
+    board_no: id,
+    contents: ""
   });
 
-  const onCommentChange = (e) => { setReply({ contents: e.target.value }); }
-  
+  const onCommentChange = (e) => { setReply((prevState) => ({...prevState, contents: e.target.value })); }
+  const onCommentKeyPress = (e) => { if (e.key === 'Enter') { handleSubmit(); } }
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    let time = { year: new Date().getFullYear(), month: new Date().getMonth() + 1, date: new Date().getDate(), hours: new Date().getHours(), minutes: new Date().getMinutes() };
-    let timestring = `${time.year}-${time.month}-${time.date}T${time.hours}:${time.minutes}:00`;
-
-    setReply({ reply_no: reply.reply_no + 1, reg_date: timestring, writer: localStorage.getItem("username") });
-    call(`/api/reply/create`, "POST", reply).then((response) => {
+    
+    call("/api/reply/create", "POST", reply).then((response) => {
       setReply(response.data);
+      window.location.reload();
     });
   };
 
-  const onDelete = (replyNo) => {
-    // console.log(replyNo);
-    call(`/api/reply/delete/${replyNo}`, "GET", null).then((response) => {
-      setList(response.data);
+  const onDelete = (reply_no) => {
+    call(`/api/reply/delete/${reply_no}`, "GET", null).then((response) => {
+      setReply(response.data);
     });
   }
 
@@ -43,80 +37,79 @@ function Board() {
   }, []);
 
   // console.log(id); 
-  // console.log(list);
+  console.log(list);
+  // console.log(reply);
   // console.log(list.member['name']);
 
   return (
     <>
-      {list &&
-        <>
-          <div className='entire'>
-            <div className='header'>
-              <a href="/home"><span class="material-symbols-rounded">chevron_left</span></a>
-              <a href="/basket"><span class="material-symbols-rounded" id='basket'>shopping_basket</span></a>
-            </div>
-            <hr />
-            <div className='main'>
-              <div className='board'>
-                <div className='user'>
-                  <img src={profile} alt='profile' />
-                  <div className='infor'>
-                    <p className='name'>{list.member.name}</p>
-                    <p className='date'>{list.reg_date.slice(0, 4)}년 {list.reg_date.slice(5, 7)}월 {list.reg_date.slice(8, 10)}일</p>
-                  </div>
-                </div>
-                <h4 className='title'>{list.title}</h4>
-                <p className='content'>{list.contents}</p> 
-                <div className='location'>
-                  <span class="material-symbols-rounded">location_on</span>
-                  <p>경상북도 구미시 대학로 62 오름관 1동 1층</p>
-                </div>
-                <div className='restaurant'>
-                  <span class="material-symbols-rounded">home</span>
-                  <p>{list.restaurant.name}</p>
-                </div>
-              </div>
-              <div className='ing'>
-                <h4>모임중인 사용자</h4>
-                <div className='users'>
-                  {list.participant && list.participant.map((item) => 
-                    <div className='user' id={item.participation_no}>
-                      <img src={profile} alt='profile' />
-                      <p className='name'>{item.participant_name}</p>
-                    </div>
-                  )}
-                </div>
+      <div className='entire'>
+        <div className='header'>
+          <a href="/home"><span class="material-symbols-rounded">chevron_left</span></a>
+          <Link to={`/menuview/${list.board_no}`}>
+            <span class="material-symbols-rounded" id='basket'>shopping_basket</span>
+          </Link>
+        </div>
+        <hr />
+        <div className='main'>
+          <div className='board'>
+            <div className='user'>
+              <img src={profile} alt='profile' />
+              <div className='infor'>
+                <p className='name'>{list.member.name}</p>
+                <p className='date'>{list.reg_date.slice(0, 14)}</p>
               </div>
             </div>
-            <hr />
-            {list.reply && list.reply.map((item) =>
-              <>
-                <div className='comment' id={item.reply_no}>
-                  <div className='user'>
-                    <img src={profile} alt='profile'/>
-                    <p>{item.writer}</p>
-                    <p className='time'>{item.reg_date.slice(0, 4)}.{item.reg_date.slice(5, 7)}.{item.reg_date.slice(8, 10)} {item.reg_date.slice(11, 13)}:{item.reg_date.slice(14, 16)}</p>
-                  </div>
-                  <p>{item.contents}</p>
-                  {/* 본인 댓글만 삭제할 수 있도록 */}
-                  { localStorage.getItem("username") == item.writer &&
-                    <button onClick={() => onDelete(item.reply_no)}>
-                      <span class="material-symbols-rounded">delete</span>
-                    </button>
-                  }
-                </div>
-                <hr />
-              </>
-            )}
+            <h4 className='title'>{list.title}</h4>
+            <p className='content'>{list.contents}</p> 
+            <div className='location'>
+              <span class="material-symbols-rounded">location_on</span>
+              <p>경상북도 구미시 대학로 62 오름관 1동 1층</p>
+            </div>
+            <div className='restaurant'>
+              <span class="material-symbols-rounded">home</span>
+              <p>{list.restaurant.name}</p>
+            </div>
           </div>
-          <form noValidate onSubmit={handleSubmit} className='input'>
-            <input placeholder='댓글을 입력하세요.' onChange={onCommentChange}/>
-            <button type="submit">
-              <span class="material-symbols-rounded">check_circle</span>
-            </button>
-          </form>
-        </>
-      }
+          <div className='ing'>
+            <h4>모임중인 사용자</h4>
+            <div className='users'>
+              {list.participant && list.participant.map((item) => 
+                <div className='user' id={item.participation_no}>
+                  <img src={profile} alt='profile' />
+                  <p className='name'>{item.participant_name}</p>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <hr />
+        {list.reply && list.reply.map((item) =>
+          <>
+            <div className='comment' id={item.reply_no}>
+              <div className='user'>
+                <img src={profile} alt='profile'/>
+                <p>{item.writer}</p>
+                <p className='time'>{item.reg_date.slice(0, 4)}.{item.reg_date.slice(5, 7)}.{item.reg_date.slice(8, 10)} {item.reg_date.slice(11, 13)}:{item.reg_date.slice(14, 16)}</p>
+              </div>
+              <p>{item.contents}</p>
+              {/* 본인 댓글만 삭제할 수 있도록 */}
+              { AppStorage.getItem("username") == item.writer &&
+                <button onClick={() => onDelete(item.reply_no)}>
+                  <span class="material-symbols-rounded">delete</span>
+                </button>
+              }
+            </div>
+            <hr />
+          </>
+        )}
+      </div>
+      <form noValidate onSubmit={handleSubmit} className='input'>
+        <input placeholder='댓글을 입력하세요.' onChange={onCommentChange} onKeyPress={onCommentKeyPress}/>
+        <button type="submit">
+          <span class="material-symbols-rounded">check_circle</span>
+        </button>
+      </form>
     </>
   );
 }

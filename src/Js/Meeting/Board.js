abd588de3ad532e3b2229a0cb80ded5754457c4d
withsme <tmdal9953@gmail.com>
2023-05-24 14:12:40 +0900

    Feat 모임 게시글 생성 페이지 카테고리 선택란 삭제 및 DB의 컬럼명과 통일

--- src/Js/Meeting/Board.js
+++ src/Js/Meeting/Board.js
@@ -64,7 +64,7 @@ function Board() {
             <p className='content'>{list.contents}</p> 
             <div className='location'>
               <span class="material-symbols-rounded">location_on</span>
-              <p>경상북도 구미시 대학로 62 오름관 1동 1층</p>
+              <p>{list.location}</p>
             </div>
             <div className='restaurant'>
               <span class="material-symbols-rounded">home</span>
